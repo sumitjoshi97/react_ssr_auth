fab48509206203b124ded2b3250df778ec8929a7
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-15 18:04:16 +0530

    client side entry point

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// importing components
+import Home from './components/Home'
+
+ReactDOM.hydrate(<Home/>, document.getElementById('root'))
