d098a4079397d46bb1d042299352abe41f90208a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-15 18:06:06 +0530

    client webpack config

---
+++ webpack.client.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const merge = require('webpack-merge')
+
+const baseConfig = require('./webpack.base')
+
+const config = {
+  //  tell webpack root file of application for client side
+  entry: './src/client/index.js',
+
+  //  tell webpack where to put output file that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
+}
+
+module.exports = merge(baseConfig, config)
