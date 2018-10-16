dcd1d8e407acdc211dce19b10a5f226015b2f19d
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-16 13:05:57 +0530

    routes updated for ssr

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -5,11 +5,14 @@ import { Route } from 'react-router-dom'
 import Home from './components/Home'
 import UsersList from './components/UsersList'
 
-export default () => {
-  return (
-    <>
-      <Route exact path="/" component={Home} />
-      <Route path="/users" component={UsersList} />
-    </>
-  )
-}
+export default [
+  {
+    path: '/',
+    component: Home,
+    exact: true
+  },
+  {
+    path: '/users',
+    component: UsersList
+  }
+]
