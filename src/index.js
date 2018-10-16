16d6f333b0799c6257c77cc475c9bb042bc0ff90
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-16 11:21:13 +0530

    adding redux to server render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import renderer from './render'
+import store from './client/store/store'
 
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send(renderer())
+app.get('*', (req, res) => {
+  const store = store()
+
+  //initialize and load data into store
+
+  //send req object to renderer
+  res.send(renderer(req, store))
 })
 
 // app listening to port 3000
