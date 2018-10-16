4c07ecd5542294b09cd089e345462d3daef07013
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-16 12:35:03 +0530

    added fetchUsers

--- src/client/components/UsersList.js
+++ src/client/components/UsersList.js
@@ -3,13 +3,17 @@ import { connect } from 'react-redux'
 import { fetchUsers } from '../actions'
 
 class UsersList extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
 
   renderUsers = () => {
     return this.props.users.map(user => <li key={user.id}>{user.name}</li>)
   }
 
   render() {
+    console.log('users list')
+    console.log(this.props)
     return <div>{this.renderUsers()}</div>
   }
 }
