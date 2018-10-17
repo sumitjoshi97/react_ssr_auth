0d2e9bdc07752da90132e4178134d7807d26165b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-17 15:49:28 +0530

    axios instance added to thunk

--- src/client/index.js
+++ src/client/index.js
@@ -3,20 +3,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
+import axios from 'axios'
 
 // import redux libraries
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 // import routes
 import Routes from './Routes'
 
-import createStore from './store/store'
-const store = createStore()
+// create axios instance to prepend before every axios call
+const axiosInstance = axios.create({
+  baseURL: '/api'
+})
 
+// create store
+const store = createStore(
+  reducers,
+  window.INITIAL_STATE,
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
+)
+
+// create rendering app
 const app = (
   <Provider store={store}>
     <Router>{renderRoutes(Routes)}</Router>
   </Provider>
 )
 
+// hydrate app for client side
 ReactDOM.hydrate(app, document.getElementById('root'))
