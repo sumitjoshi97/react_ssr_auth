ee559feb6ec853fc308217cb3e09402d606a4d3d
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-17 15:46:58 +0530

    updated fr proxy request

--- src/client/store/store.js
+++ src/client/store/store.js
@@ -2,4 +2,16 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 
-export default () => createStore(reducers, {}, applyMiddleware(thunk))
+export default (req) => {
+  const axiosInstance = axios.create({
+    baseURL: 'http://react-ssr-api.herokuapp.com',
+    headers: {cookie: req.get('cookie') || ''}
+  })
+  
+  store = createStore(
+    reducers,
+    window.INITIAL_STATE,
+    applyMiddleware(thunk.withExtraArgument(axiosInstance))
+  )
+  return store
+}
