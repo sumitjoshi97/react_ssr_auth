5233737694e97591011adfeda3f427dbd416e66b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-17 15:46:39 +0530

    refactored

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../actions'
-import createStore from '../store/store'
-
-const store = createStore()
 
 class UsersList extends Component {
-  componentDidMount() {
-    this.props.fetchUsers()
-  }
+  // componentDidMount() {
+  //   this.props.fetchUsers()
+  // }
 
   renderUsers = () => {
     return this.props.users.map(user => <li key={user.id}>{user.name}</li>)
@@ -23,11 +20,14 @@ const mapStateToProps = state => ({
   users: state.users
 })
 
-export const loadData = () => {
+export const loadData = store => {
   store.dispatch(fetchUsers())
 }
 
-export default connect(
-  mapStateToProps,
-  { fetchUsers }
-)(UsersList)
+export default {
+  component: connect(
+    mapStateToProps,
+    { fetchUsers }
+  )(UsersList),
+  loadData: loadData
+}
