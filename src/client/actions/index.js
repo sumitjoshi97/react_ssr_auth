6fe538aa2bb2c373155c7127cfbfb942a171d983
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-18 11:22:30 +0530

    fetch admins action added

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,6 +1,12 @@
+// action types consts
 export const FETCH_USERS = 'FETCH_USERS'
 export const FETCH_CURRENT_USER = 'FETCH_CURRENT_USER'
+export const FETCH_ADMINS = 'FETCH_ADMINS'
 
+
+// action creators
+
+//action to fetch all users 
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get('/users')
   dispatch({
@@ -9,12 +15,21 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
   })
 }
 
-
-export const fetchCurrentUser = () => async(dispatch, getState, api) => {
+// action to fetch current logged-in user
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
   const res = await api.get('/current_user')
 
   dispatch({
     type: FETCH_CURRENT_USER,
+    payload: res
+  })
+}
+
+export const fetchAdmins = () => async (dispatch, getaState, api) => {
+  const res = await api.get('/admins')
+
+  dispatch({
+    type: FETCH_ADMINS,
     payload: res
   })
 }
