3b62737e8744529e28e662fae3c46883d9086d99
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-18 11:22:01 +0530

    markup modified

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-      <div>
-        Home Component updated
-        <button onClick={() => console.log('pressed')}>Press me</button>
+      <div className="center-align" style={{marginTop: '200px'}}>
+        <h3>Welcome</h3>
+        <p>Check my first SSR app</p> 
       </div>
     )
   }
