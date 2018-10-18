e2f25be7c677c0d37b291debb6a1d16436f7e89a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-18 15:47:48 +0530

    port changed to 3000

--- src/index.js
+++ src/index.js
@@ -56,4 +56,4 @@ app.get('*', (req, res) => {
 })
 
 // app listening to port 3000
-app.listen(5000, () => console.log('Listening on port 5000'))
+app.listen(3000, () => console.log('Listening on port 3000'))
