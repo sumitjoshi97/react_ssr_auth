ae31cae6363c2deb63cbbff0f0e909a55225cec7
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-18 11:22:17 +0530

    added links

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Header = () => {
+const Header = ({ auth }) => {
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="api/auth/google">Login</a>
+  )
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-    </div>
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">Home</Link>
+        <ul className="right">
+          <li><Link to="/admin">admin</Link></li>
+          <li><Link to="/users">users</Link></li>
+          {authButton}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
-export default Header
+const mapStateToProps = ({ auth }) => ({
+  auth
+})
+
+export default connect(mapStateToProps)(Header)
